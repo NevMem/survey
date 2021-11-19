c8f3971991ee756651ca70c067af8bc3d25ed29d
NevMem <memlolkek@gmail.com>
2021-11-20 01:39:53 +0300

    Fix

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Root';
+import Root from './Root';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
