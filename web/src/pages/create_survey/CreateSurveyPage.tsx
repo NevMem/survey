2b10c7ca8bb5fbb033e11adbf742e1dffd3923c3
NevMem <memlolkek@gmail.com>
2021-11-24 00:56:25 +0300

    Working on modal for selection of question type

--- web/src/pages/create_survey/CreateSurveyPage.tsx
+++ web/src/pages/create_survey/CreateSurveyPage.tsx
@@ -1,5 +1,5 @@
 import { observer } from 'mobx-react-lite';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import styled from 'styled-components';
 import Text from '../../components/text/Text';
 import createSurveyService, { CreateSurveyService } from '../../service/create_survey/CreateSurveyService';
@@ -37,11 +37,88 @@ const Input = styled.input`
     font-size: 1.5em;
 `;
 
+const Modal = styled.div`
+    margin: 15% auto;
+    width: 400px;
+    background-color: ${props => props.theme.background};
+    border-radius: 8px;
+    padding: 16px;
+    clickable: true;
+    z-index: 2;
+`;
+
+const ModalHeader = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+const ModalBody = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding-top: 20px;
+    padding-bottom: 20px;
+`;
+
+const ModalActions = styled.div`
+    display: flex;
+    flex-direction: row-reverse;
+    column-gap: 10px;
+`;
+
+const Selector = styled.select`
+    outline: none;
+    padding: 8px;
+`;
+
 const AddQuestionSection = () => {
+    const [opened, setOpened] = useState(false);
+    const openNewQuestionModal = () => {
+        setOpened(true);
+    }
+    const closeNewQuestionModal = () => {
+        setOpened(false);
+    }
+
+    const selectorChanged = (event: any) => {
+        const newValue = event.target.value;
+    }
+
     return (
-        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around', padding: '16px', cursor: 'pointer'}}>
-            <img src={plusIcon} alt='plus icon' width='32px' height='32px' />
-        </div>
+        <Fragment>
+            <div style={{
+                display: opened ? 'block': 'none',
+                position: 'fixed',
+                zIndex: '1',
+                left: '0',
+                top: '0',
+                right: '0',
+                bottom: '0',
+                backgroundColor: 'rgba(0, 0, 0, 0.2)'
+            }} onClick={closeNewQuestionModal}>
+                <Modal onClick={(event) => {event.preventDefault(); event.stopPropagation();}}>
+                    <ModalHeader>
+                        <Text>Новый опрос</Text>
+                        <div>&times;</div>
+                    </ModalHeader>
+                    <ModalBody>
+                        <Selector onChange={selectorChanged}>
+                            <option>-</option>
+                            <option>a</option>
+                            <option>b</option>
+                            <option>c</option>
+                        </Selector>
+                    </ModalBody>
+                    <ModalActions>
+                        <GeneralButton>Ок</GeneralButton>
+                        <GeneralButton secondary onClick={closeNewQuestionModal}>Отмена</GeneralButton>
+                    </ModalActions>
+                </Modal>
+            </div>
+            <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around', padding: '16px', cursor: 'pointer'}} onClick={openNewQuestionModal}>
+                <img src={plusIcon} alt='plus icon' width='32px' height='32px' />
+            </div>
+        </Fragment>
     );
 }
 
