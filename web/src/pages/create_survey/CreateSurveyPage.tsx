89fb19bf14cca66813f8bc537928237e1cedf306
NevMem <memlolkek@gmail.com>
2021-11-24 13:17:46 +0300

    better

--- web/src/pages/create_survey/CreateSurveyPage.tsx
+++ web/src/pages/create_survey/CreateSurveyPage.tsx
@@ -5,6 +5,7 @@ import Text from '../../components/text/Text';
 import createSurveyService, { CreateSurveyService } from '../../service/create_survey/CreateSurveyService';
 import plusIcon from '../../images/base/plus.svg';
 import GeneralButton from '../../components/button/GeneralButton';
+import Question, { RatingQuestion } from '../../data/Question';
 
 const PageWrapper = styled.div`
     margin: 20px;
@@ -71,6 +72,54 @@ const Selector = styled.select`
     padding: 8px;
 `;
 
+const RatingQuestionBlock = (props: {setQuestion: (question: Question | undefined) => void}) => {
+    const [min, setMin] = useState(0);
+    const [max, setMax] = useState(10);
+    const [name, setName] = useState('');
+
+    const updateQuestion = (newMin?: number, newMax?: number, newName?: string) => {
+        props.setQuestion({title: newName ?? name, min: newMin ?? min, max: newMax ?? max} as RatingQuestion);
+    }
+
+    const changeMin = (event: any) => {
+        setMin(event.target.value | 0);
+        updateQuestion(event.target.value | 0);
+    }
+
+    const changeMax = (event: any) => {
+        setMax(event.target.value | 0);
+        updateQuestion(undefined, event.target.value | 0);
+    }
+
+    const changeName = (event: any) => {
+        setName(event.target.value);
+        updateQuestion(undefined, undefined, event.target.value);
+    }
+
+    return (
+        <div style={{paddingTop: '10px', paddingBottom: '10px'}}>
+            <Text>Вопрос:</Text>
+            <Input value={name} onChange={changeName} />
+            <Text>Минимальный рейтинг:</Text>
+            <Input value={min} onChange={changeMin} />
+            <Text>Максимальный рейтинг:</Text>
+            <Input value={max} onChange={changeMax} />
+        </div>
+    );
+}
+
+const QuestionCreationBlock = (props: {type: string, setQuestion: (question: Question | undefined) => void}) => {
+    if (props.type === '-') {
+        return null;
+    }
+    if (props.type === 'rating') {
+        return (
+            <RatingQuestionBlock setQuestion={props.setQuestion} />
+        );
+    }
+    return null;
+}
+
 const AddQuestionSection = () => {
     const [opened, setOpened] = useState(false);
     const openNewQuestionModal = () => {
@@ -80,9 +129,17 @@ const AddQuestionSection = () => {
         setOpened(false);
     }
 
+    const options = [
+        '-', 'rating', 'stars', 'text',
+    ]
+    const [selectedOption, setSelectedOption] = useState(options[0]);
+
     const selectorChanged = (event: any) => {
         const newValue = event.target.value;
+        setSelectedOption(newValue)
     }
+
+    const [currentQuestion, setCurrentQuestion] = useState<Question | undefined>(undefined);
 
     return (
         <Fragment>
@@ -102,15 +159,15 @@ const AddQuestionSection = () => {
                         <div>&times;</div>
                     </ModalHeader>
                     <ModalBody>
-                        <Selector onChange={selectorChanged}>
-                            <option>-</option>
-                            <option>a</option>
-                            <option>b</option>
-                            <option>c</option>
+                        <Selector value={selectedOption} onChange={selectorChanged}>
+                            {options.map((value) => {
+                                return <option key={value}>{value}</option>;                                
+                            })}
                         </Selector>
+                        <QuestionCreationBlock type={selectedOption} setQuestion={setCurrentQuestion} />
                     </ModalBody>
                     <ModalActions>
-                        <GeneralButton>Ок</GeneralButton>
+                        <GeneralButton>Добавить</GeneralButton>
                         <GeneralButton secondary onClick={closeNewQuestionModal}>Отмена</GeneralButton>
                     </ModalActions>
                 </Modal>
