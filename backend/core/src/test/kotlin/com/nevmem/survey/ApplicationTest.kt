b19733c22e2ba002720dac20eb96049d2e43dd45
NevMem <memlolkek@gmail.com>
2021-12-06 02:42:48 +0300

    Betetr

--- backend/core/src/test/kotlin/com/nevmem/survey/ApplicationTest.kt
+++ backend/core/src/test/kotlin/com/nevmem/survey/ApplicationTest.kt
@@ -1,23 +1,10 @@
 package com.nevmem.survey
 
-import io.ktor.routing.*
+import com.nevmem.survey.plugins.configureRouting
 import io.ktor.http.*
-import io.ktor.serialization.*
-import io.ktor.features.*
-import io.micrometer.prometheus.*
-import io.ktor.metrics.micrometer.*
-import io.ktor.auth.*
-import io.ktor.util.*
-import io.ktor.auth.jwt.*
-import com.auth0.jwt.JWT
-import com.auth0.jwt.JWTVerifier
-import com.auth0.jwt.algorithms.Algorithm
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import kotlin.test.*
 import io.ktor.server.testing.*
-import com.example.plugins.*
+import org.junit.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
