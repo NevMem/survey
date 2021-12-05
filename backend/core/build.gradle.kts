12f01d8654ef3957db80ebc27ced8ad9f0271dd6
NevMem <memlolkek@gmail.com>
2021-12-06 01:35:37 +0300

    Initializing backend

---
+++ backend/core/build.gradle.kts
@@ -0,0 +1,33 @@
+val ktor_version: String by project
+val kotlinVersion: String by project
+val logback_version: String by project
+val prometeus_version : String by project
+
+plugins {
+    application
+    kotlin("jvm")
+    id("org.jetbrains.kotlin.plugin.serialization")
+}
+
+group = "com.nevmem.survey"
+version = "0.0.1"
+application {
+    mainClass.set("com.nevmem.survey.SurveyApplicationKt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-serialization:$ktor_version")
+    implementation("io.ktor:ktor-metrics-micrometer:$ktor_version")
+    implementation("io.micrometer:micrometer-registry-prometheus:$prometeus_version")
+    implementation("io.ktor:ktor-auth:$ktor_version")
+    implementation("io.ktor:ktor-auth-jwt:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
+}
