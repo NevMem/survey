14c1bcd127596971b887ef2fa2654e2fe272ec1d
NevMem <memlolkek@gmail.com>
2021-12-10 02:02:44 +0300

    Some dependencies

--- backend/core/src/main/kotlin/com/nevmem/survey/routing/v1/users.kt
+++ backend/core/src/main/kotlin/com/nevmem/survey/routing/v1/users.kt
@@ -1,12 +1,21 @@
 package com.nevmem.survey.routing.v1
 
+import com.nevmem.survey.data.request.auth.LoginRequest
 import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
 import io.ktor.request.receiveOrNull
+import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.post
 
 fun Route.users() {
     post("login") {
         val request = call.receiveOrNull<LoginRequest>()
+        if (request == null) {
+            call.respond(HttpStatusCode.BadRequest)
+            return@post
+        }
+
+
     }
 }
