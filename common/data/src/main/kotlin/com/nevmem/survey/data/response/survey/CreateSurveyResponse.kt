566c6974ad411d18ae593cf208eb83b93fc60d39
NevMem <memlolkek@gmail.com>
2022-01-01 20:04:49 +0300

    Better

--- common/data/src/main/kotlin/com/nevmem/survey/data/response/survey/CreateSurveyResponse.kt
+++ common/data/src/main/kotlin/com/nevmem/survey/data/response/survey/CreateSurveyResponse.kt
@@ -2,17 +2,20 @@ package com.nevmem.survey.data.response.survey
 
 import com.nevmem.survey.Exported
 import com.nevmem.survey.data.survey.Survey
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 @Exported
 sealed class CreateSurveyResponse {
     @Serializable
+    @SerialName("success")
     data class CreateSurveySuccess(
         val survey: Survey,
     ): CreateSurveyResponse()
 
     @Serializable
+    @SerialName("error")
     data class CreateSurveyError(
         val message: String,
     ): CreateSurveyResponse()
