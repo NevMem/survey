7ab144fa346d12f272811670e546b900e5b11162
NevMem <memlolkek@gmail.com>
2022-01-02 20:09:27 +0300

    More content on profile page

--- web/src/pages/profile/ProfilePage.tsx
+++ web/src/pages/profile/ProfilePage.tsx
@@ -8,6 +8,9 @@ import authService from '../../service/authorization/AuthorizationService';
 import Text from '../../components/text/Text';
 import Card from '../../app/card/Card';
 import SpacedColumn from '../../app/layout/SpacedColumn';
+import Badge from '../../components/badge/Badge';
+import SpaceBetweenReversedRow from '../../app/layout/SpaceBetweenReversedRow';
+import GeneralButton from '../../components/button/GeneralButton';
 
 const ProfileWrapper = (props: {user: User | undefined, error: string | undefined, loading?: boolean}) => {
     if (props.loading) {
@@ -28,14 +31,29 @@ const ProfileWrapper = (props: {user: User | undefined, error: string | undefine
 
     if (props.user !== undefined) {
         const user = props.user;
+
+        const logout = () => {
+            authService.logout();
+        };
+
         return (
             <Card>
                 <SpacedColumn rowGap={16}>
                     <Text header>Профиль</Text>
-                    <Text large>{user.name}</Text>
-                    <Text large>{user.surname}</Text>
-                    <Text large>{user.login}</Text>
+                    <Text large>Имя: {user.name}</Text>
+                    <Text large>Фамилия: {user.surname}</Text>
+                    <Text large>Никнейм: {user.login}</Text>
+                    <Text large>Почта: {user.email}</Text>
+                    <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', columnGap: '16px'}}>
+                        <Text>Роли:</Text>
+                        {user.roles.map((role, index) => {
+                            return <Badge success key={index}>{role.id}</Badge>;
+                        })}
+                    </div>
                 </SpacedColumn>
+                <SpaceBetweenReversedRow>
+                    <GeneralButton secondary onClick={logout}>Выйти</GeneralButton>
+                </SpaceBetweenReversedRow>
             </Card>
         );
     }
