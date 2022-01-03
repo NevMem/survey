fcff9148ce353a413e837c296e60270eadf51ba7
NevMem <memlolkek@gmail.com>
2022-01-03 16:02:15 +0300

    Working on env exporting

--- backend/core/docker/runner.sh
+++ backend/core/docker/runner.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-# source vars.sh
-# ls -la
+source env.sh
 /core/bin/core
---
+++ tools/bundle_env_vars.py
@@ -0,0 +1,28 @@
+import argparse
+import json
+import os
+import sys
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--configuration", required=True)
+    parser.add_argument("--output", required=True)
+    args = parser.parse_args()
+
+    data = json.loads(open(args.configuration).read())
+    
+    with open(args.output, 'w') as out:
+        for required in data['required']:
+            if required not in os.environ:
+                print('Env variiable not set:', required)
+                sys.exit(1)
+            out.write(f"export {required}={os.environ[required]}\n")
+
+        for other in data['other']:
+            if other in os.environ:
+                out.write(f"export {other}={os.environ[other]}\n")
+
+
+if __name__ == '__main__':
+    main()
