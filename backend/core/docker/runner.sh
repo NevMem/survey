f612969ed3eb768914fbf354eb0fa5b452298aa1
NevMem <memlolkek@gmail.com>
2022-01-15 17:44:40 +0300

    Try use ssl

--- backend/core/docker/runner.sh
+++ backend/core/docker/runner.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
 source env.sh
+
+set -eu
+
+cp nginx-production-config.conf /etc/nginx/sites-enabled/ethno.conf
+cat /etc/nginx/sites-enabled/ethno.conf
+rm /etc/nginx/sites-enabled/default
+
+echo "starting nginx"
+nginx
+echo "done"
+
+echo "starting core"
 /core/bin/core
