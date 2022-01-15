85938616ee2db2cb069eed7ed0b062a040e5bb46
NevMem <memlolkek@gmail.com>
2022-01-15 18:04:59 +0300

    Try more

--- backend/core/docker/runner.sh
+++ backend/core/docker/runner.sh
@@ -8,6 +8,13 @@ cp nginx-production-config.conf /etc/nginx/sites-enabled/ethno.conf
 cat /etc/nginx/sites-enabled/ethno.conf
 rm /etc/nginx/sites-enabled/default
 
+ls -la
+
+cp certificate_full_chain.pem /etc/nginx/certificate_full_chain.pem
+cp private_key.pem /etc/nginx/private_key.pem
+
+ls -la /etc/nginx/
+
 echo "starting nginx"
 nginx
 echo "done"
