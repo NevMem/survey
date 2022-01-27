98cd17a387941642b25fea8ded5984bd9070bccb
NevMem <memlolkek@gmail.com>
2022-01-28 02:13:11 +0300

    Some working on new rolling scheme

--- tools/roll.py
+++ tools/roll.py
@@ -1,3 +1,5 @@
+import argparse
+import json
 from machines import Machine, get_machines_for_service
 from typing import List
 import subprocess as sp
@@ -32,17 +34,61 @@ class RemoteCommandExecutor:
         return self.launch_command(['docker', '-v']) == 0
 
     def install_docker(self):
-        self.launch_command(['sudo', 'apt-get', 'update'])
-        self.launch_command(['sudo', 'apt-get', 'install', 'ca-certificates', 'curl', 'gnupg', 'lsb-release'])
-        self.launch_command(['bin/bash', 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg'])
-        # self.launch_command(['sudo', 'apt-get', 'update'])
-        # self.launch_command(['sudo', 'apt-get', 'install', 'docker-ce', 'docker-ce-cli', 'containerd.io'])
+        self.launch_command(['curl', '-fsSL', 'https://get.docker.com', '-o', 'get-docker.sh'])
+        self.launch_command(['sudo', 'sh', 'get-docker.sh'])
 
+
+def deploy(executor: RemoteCommandExecutor, image_tag: str, machine: Machine, ports: List[int], container_name: str):
+    executor.launch_command(['sudo', 'docker', 'pull', image_tag])
+    executor.launch_command(['sudo', 'docker', 'stop', container_name])
+    executor.launch_command(['sudo', 'docker', 'rm', container_name])
+
+    ports_arguments = []
+    for port in ports:
+        ports_arguments.append('-p')
+        ports_arguments.append(f"{port}:{port}")
+
+    executor.launch_command(['sudo', 'docker', 'run', '-d', *ports_arguments, '--name', container_name, image_tag])
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("service")
+    parser.add_argument("services_file")
+    parser.add_argument("identity_file")
+    return parser.parse_args()
 
 
 def main():
+    args = parse_args()
+
+    with open(args.services_file, 'r') as inp:
+        services = json.load(inp)
+
+    service = args.service
+
+    assert service in services
+
+    service_config = services[service]
+
     machines = get_machines_list()
-    print(machines)
+    
+    for machine in machines:
+        index = 0
+        for tag in machine.tags:
+            if tag != service:
+                continue
+            index += 1
+            executor = RemoteCommandExecutor('nevmem', machine.ip, args.identity_file)
+            if not executor.is_docker_installer():
+                executor.install_docker()
+            deploy(
+                executor=executor,
+                image_tag=service_config['container-tag'],
+                machine=machine,
+                ports=service_config['ports'],
+                container_name=f"{service}-{index}",
+            )
 
 
 if __name__ == '__main__':
