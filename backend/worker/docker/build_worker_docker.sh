c20b80167885f39373fa083cc3d394f435d271b0
NevMem <memlolkek@gmail.com>
2022-01-28 01:27:00 +0300

    Better

--- backend/worker/docker/build_worker_docker.sh
+++ backend/worker/docker/build_worker_docker.sh
@@ -8,7 +8,7 @@ cd ./backend/worker/docker
 
 cp ../build/distributions/worker-0.0.1.zip ./tmp/
 
-python3 ../tools/bundle_env_vars.py ./env_vars_config.sh ./env.sh
+python3 ../../../tools/bundle_env_vars.py ./env_vars_config.sh ./env.sh
 
 echo cr.yandex/crp9sskk3fu52ukovaco/ethno-worker:$(git rev-parse --short HEAD)
 docker build . --network=host -t cr.yandex/crp9sskk3fu52ukovaco/ethno-worker:$(git rev-parse --short HEAD)
