5a772c1715de6a2e2769859562a94db55ed57947
NevMem <memlolkek@gmail.com>
2022-01-28 01:30:52 +0300

    Better

--- backend/worker/docker/build_worker_docker.sh
+++ backend/worker/docker/build_worker_docker.sh
@@ -3,12 +3,12 @@ set -eu
 mkdir tmp
 
 cd ../../../
-./gradlew backend:worker:build
+./gradlew backend:worker:build --no-daemon
 cd ./backend/worker/docker
 
 cp ../build/distributions/worker-0.0.1.zip ./tmp/
 
-python3 ../../../tools/bundle_env_vars.py ./env_vars_config.sh ./env.sh
+python3 ../../../tools/bundle_env_vars.py --configuration ./env_vars_config.json --output ./env.sh
 
 echo cr.yandex/crp9sskk3fu52ukovaco/ethno-worker:$(git rev-parse --short HEAD)
 docker build . --network=host -t cr.yandex/crp9sskk3fu52ukovaco/ethno-worker:$(git rev-parse --short HEAD)
