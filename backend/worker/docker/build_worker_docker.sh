19e69b757c5022eb8a629005c900804c14ee61b1
NevMem <memlolkek@gmail.com>
2022-01-28 01:14:51 +0300

    Worker workflow

--- backend/worker/docker/build_worker_docker.sh
+++ backend/worker/docker/build_worker_docker.sh
@@ -4,7 +4,7 @@ cp ../backend/worker/build/distributions/worker-0.0.1.zip ./tmp/
 
 python3 ../tools/bundle_env_vars.py ./env_vars_config.sh ./env.sh
 
-docker build . --network=host -t cr.yandex/crp9sskk3fu52ukovaco/ethno-metrics:$(git rev-parse --short HEAD)
-docker push cr.yandex/crp9sskk3fu52ukovaco/ethno-metrics:$(git rev-parse --short HEAD)
+docker build . --network=host -t cr.yandex/crp9sskk3fu52ukovaco/ethno-worker:$(git rev-parse --short HEAD)
+docker push cr.yandex/crp9sskk3fu52ukovaco/ethno-worker:$(git rev-parse --short HEAD)
 
 rm -rf tmp
