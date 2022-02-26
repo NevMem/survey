6f3389c2dee602e68245b0ea446d6344f1292071
NevMem <memlolkek@gmail.com>
2022-02-26 04:13:22 +0300

    Kke

--- android/app/src/main/java/com/nevmem/survey/ui/home/HomeScreen.kt
+++ android/app/src/main/java/com/nevmem/survey/ui/home/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.nevmem.survey.ui.home
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
