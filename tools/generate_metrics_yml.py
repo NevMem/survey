c2354c0c5130009e19d8d4a2aff553b0a8f99dee
NevMem <memlolkek@gmail.com>
2022-03-26 15:42:33 +0300

    Fix some shit

--- tools/generate_metrics_yml.py
+++ tools/generate_metrics_yml.py
@@ -10,7 +10,14 @@ scrape_configs:
   - job_name: 'core'
     scrape_interval: 5s
     static_configs:
-      - targets: [{% for instance in instances %}'{{instance}}'{{ ", " if not loop.last else "" }}{% endfor %}]
+      - targets: [{% for instance in core_instances %}'{{instance}}'{{ ", " if not loop.last else "" }}{% endfor %}]
+        labels:
+          group: 'production'
+
+  - job_name: 'worker'
+    scrape_interval: 5s
+    static_configs:
+      - targets: [{% for instance in worker_instances %}'{{instance}}'{{ ", " if not loop.last else "" }}{% endfor %}]
         labels:
           group: 'production'
 
@@ -18,11 +25,13 @@ scrape_configs:
 
 
 def main():
-    machines = get_machines_for_service('core')
-    instances = list(map(lambda x: x.ip, machines))
+    core_machines = get_machines_for_service('core')
+    worker_machines = get_machines_for_service('worker')
+    core_instances = list(map(lambda x: x.ip, core_machines))
+    worker_instances = list(map(lambda x: x.ip, worker_machines))
 
     with open('tmp/prometheus.yml', 'w') as out:
-        out.write(TEMPLATE.render(instances=instances))
+        out.write(TEMPLATE.render(core_instances=core_instances, worker_instances=worker_instances))
 
 
 if __name__ == '__main__':
