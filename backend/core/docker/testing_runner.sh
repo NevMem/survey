667f5efe2c13c611504354949f7bf4d3c07bbfb6
NevMem <memlolkek@gmail.com>
2022-03-27 15:34:49 +0300

    try fix tests

--- backend/core/docker/testing_runner.sh
+++ backend/core/docker/testing_runner.sh
@@ -5,6 +5,9 @@ set -eu
 ./wait-for-it.sh s3local:8000 -t 60
 ./wait-for-it.sh database:5432 -t 60
 
+echo "starting core"
+core-0.0.1/bin/core
+
 cp nginx-config.conf /etc/nginx/sites-enabled/ethno.conf
 cat /etc/nginx/sites-enabled/ethno.conf
 rm /etc/nginx/sites-enabled/default
@@ -12,6 +15,3 @@ rm /etc/nginx/sites-enabled/default
 echo "starting nginx"
 nginx
 echo "done"
-
-echo "starting core"
-core-0.0.1/bin/core
