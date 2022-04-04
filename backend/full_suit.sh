1a9edae1c8b1e765457eda400de030e4451b7e61
NevMem <memlolkek@gmail.com>
2022-04-04 14:01:51 +0300

    Try fix tests

--- backend/full_suit.sh
+++ backend/full_suit.sh
@@ -4,6 +4,7 @@ docker-compose down
 cd ../
 ./gradlew backend:core:build
 ./gradlew backend:worker:build
+./gradlew backend:push:build
 cd backend
 docker-compose build
 docker-compose up -d database
