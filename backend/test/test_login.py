eb4e957c382ce4089b6bf76c8244cfbd138f6c16
NevMem <memlolkek@gmail.com>
2022-04-25 23:22:42 +0300

    Fix tests

--- backend/test/test_login.py
+++ backend/test/test_login.py
@@ -1,12 +1,13 @@
 from client import Client
 from client_fixture import *
+from v2.user_fixture import *
 
-def test_login(client: Client):
-    assert client.login('admin', 'password').status_code == 200
+def test_login(client: Client, user: User):
+    assert client.login(user.login, user.password).status_code == 200
 
-    assert client.login('admin1', 'password').status_code != 200
-    assert client.login('admin2', 'password').status_code != 200
-    assert client.login('admin3', 'password').status_code != 200
+    assert client.login(user.login + '1', user.password).status_code != 200
+    assert client.login(user.login + '2', user.password).status_code != 200
+    assert client.login(user.login + '3', user.password).status_code != 200
 
     assert client.login('a1', 'password').status_code != 200
     assert client.login('ksfnfn', 'password').status_code != 200
