d9a9fca1d7a1403a56299c807eaa488f00ebb2b5
NevMem <memlolkek@gmail.com>
2022-05-05 01:49:44 +0300

    Try another fix for nginx

--- backend/core/docker/runner.sh
+++ backend/core/docker/runner.sh
@@ -4,16 +4,15 @@ source env.sh
 
 set -eu
 
-cp nginx-production-config.conf /etc/nginx/sites-enabled/ethno.conf
-cat /etc/nginx/sites-enabled/ethno.conf
+cp nginx-production-config.conf /etc/nginx/nginx.conf
 rm /etc/nginx/sites-enabled/default
-
-ls -la
 
 cp certificate_full_chain.pem /etc/nginx/certificate_full_chain.pem
 cp private_key.pem /etc/nginx/private_key.pem
 
 ls -la /etc/nginx/
+
+cat /etc/nginx/nginx.conf
 
 echo "starting nginx"
 nginx
