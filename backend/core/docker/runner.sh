35c1e1241626ba2d3905b498ef21ac04779f9697
NevMem <memlolkek@gmail.com>
2022-05-09 17:27:00 +0300

    Get back ssl for core

--- backend/core/docker/runner.sh
+++ backend/core/docker/runner.sh
@@ -7,8 +7,8 @@ set -eu
 cp nginx-production-config.conf /etc/nginx/nginx.conf
 rm /etc/nginx/sites-enabled/default
 
-# cp certificate_full_chain.pem /etc/nginx/certificate_full_chain.pem
-# cp private_key.pem /etc/nginx/private_key.pem
+cp certificate_full_chain.pem /etc/nginx/certificate_full_chain.pem
+cp private_key.pem /etc/nginx/private_key.pem
 
 ls -la /etc/nginx/
 
