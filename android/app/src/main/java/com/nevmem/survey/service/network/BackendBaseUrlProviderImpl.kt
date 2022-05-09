71069923683f29ca73257fedf99c555d9f1c71a5
NevMem <memlolkek@gmail.com>
2022-05-09 19:21:38 +0300

    Better

--- android/app/src/main/java/com/nevmem/survey/service/network/BackendBaseUrlProviderImpl.kt
+++ android/app/src/main/java/com/nevmem/survey/service/network/BackendBaseUrlProviderImpl.kt
@@ -7,7 +7,6 @@ class BackendBaseUrlProviderImpl(
     private val settingsService: SettingsService,
 ) : BackendBaseUrlProvider {
     override suspend fun provideBaseUrl(): String {
-//        return "http://192.168.1.66"
         return if (settingsService.isHttpBackendUrlEnabled.value) {
             "http://ethnosurvey.com"
         } else {
