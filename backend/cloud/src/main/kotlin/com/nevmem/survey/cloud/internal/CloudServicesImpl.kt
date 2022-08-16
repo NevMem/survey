302d21b21c269fb7b5df7379011b3365ef47e0f4
NevMem <memlolkek@gmail.com>
2022-08-16 13:04:42 +0300

    Ktlint

--- backend/cloud/src/main/kotlin/com/nevmem/survey/cloud/internal/CloudServicesImpl.kt
+++ backend/cloud/src/main/kotlin/com/nevmem/survey/cloud/internal/CloudServicesImpl.kt
@@ -6,7 +6,6 @@ import com.nevmem.survey.env.EnvVars
 import io.ktor.client.HttpClient
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.json.serializer.KotlinxSerializer
-import io.ktor.client.features.logging.DEFAULT
 import io.ktor.client.features.logging.LogLevel
 import io.ktor.client.features.logging.Logging
 import io.ktor.client.request.post
